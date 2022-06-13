e2973f0a106a435a4a6ef660b1ab205eace606d5
roberttijani <roberttijani87@gmail.com>
2022-06-13 07:42:40 +0700

    update lurr

--- conn.php
+++ conn.php
@@ -3,7 +3,7 @@
 				"host"	=> "localhost",
 				"user"	=> "root",
 				"pass"	=> "",
-				"db"	=> "ayamin"];
+				"db"	=> "peternakan"];
 				
 	$link = new mysqli($db_conf['host'], $db_conf['user'], $db_conf['pass'], $db_conf['db']);
 ?>
